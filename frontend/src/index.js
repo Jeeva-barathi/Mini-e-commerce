4fb7e88618ed0800634926c57faea773c377bcff
Jeevabarathi <116498801+Jeeva-barathi@users.noreply.github.com>
2025-05-26 10:36:12 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
-
 const backend = "https://mini-e-com-backend.onrender.com";
-backend.render(
+backend.render();
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
